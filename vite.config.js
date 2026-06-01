b4876be08691b7e9955b2481e5719846329a1a27
AryanDatta <62801202+AryanDatta@users.noreply.github.com>
2026-06-01 19:21:01 +0530

    ca

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
-import React from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    base: process.env.VITE_BASE_PATH || "/react-fe-bsn",
+  plugins: [react()],
+  // base: "/react-fe-bsn",   // ← commented out for Vercel (not needed)
 });
